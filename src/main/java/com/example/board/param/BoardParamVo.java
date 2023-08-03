99ec31a966cf2ab9f69135179675d04704c5fc09
Naejin Nam <salvia1223@naver.com>
2023-08-03 17:52:14 +0900

    #5 대댓글기능 구현완료
    왜 pSeq로 보내면 오류가 생겼을까 하는 의문.(글자수의 영향?)

--- src/main/java/com/example/board/param/BoardParamVo.java
+++ src/main/java/com/example/board/param/BoardParamVo.java
@@ -5,11 +5,18 @@ import lombok.Data;
 @Data
 public class BoardParamVo {
     private String boardSeq;
+
     private String userId;
+
     private String up;
+
     private String content;
-    private String pCommentSeq;
+
+    private String parentSeq;
+
     private String lvl;
+
     private String categoryNmEn;
+
 
 }
